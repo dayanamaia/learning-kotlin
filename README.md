42aaedfc1e5d1c4734ac3f78410257eaa78b5449
Dayana Maia <dayrmaia@gmail.com>
2020-12-12 21:43:54 -0300

    about collections

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 - documentação Kotlin - [link](https://kotlinlang.org/docs/reference/kotlin-doc.html)
 - documentação Kotlin para Android - [link](https://kotlinlang.org/docs/reference/android-overview.html), [link](https://developer.android.com/reference/kotlin/packages)
 - Primeiros passos Kotlin para android - [link](https://developer.android.com/kotlin)
+- JetBrains Academy - kotlin - [link](https://hyperskill.org/learn/step/4350)
+- Learning Kotlin by example - [link](https://play.kotlinlang.org/byExample/overview)
---
+++ src/aboutCollections/introduction.kt
@@ -0,0 +1,38 @@
+package aboutCollections
+
+/*
+*   List
+*   Array
+*   Set
+*   Hashmap
+* */
+
+fun main() {
+
+    val l1 = listOf(10, 8, 4, 2, 9, 32, 36) // immutable
+    val l2 = mutableListOf("London", "Paris", "Helsinki", "Havana") // mutable
+    val a1 = arrayListOf<String>() // mutanle
+
+    val s1 = setOf(10, 20, 10) // sets are collections where repetition and order are ignored
+    val s2 = mutableSetOf<Int>() // sets are collections where repetition and order are ignored
+
+    val h1 = hashMapOf<String, String>(Pair("key", "value"), Pair("France", "Paris"))
+
+    val m1 = mapOf<String, String>()
+    val m2 = mutableMapOf<String, String>()
+
+
+    l2.add("Valeta")
+    a1.add("categoria")
+    s2.add(10)
+    s2.add(10)
+
+    println(l1)
+    println(l2)
+    println(a1)
+
+    println(s1.size)
+    println(s2.size)
+    println(h1)
+    println(h1["France"])
+}
